216a69929cbdbf691ffd5804ef16b9574460ef3b
Akshit Bansal <akshit.bansal755@gmail.com>
2022-10-13 15:56:46 +0530

    added ssl verification to axios

--- api/lib/axios.js
+++ api/lib/axios.js
@@ -1,7 +1,19 @@
 const axios = require('axios');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 
 const AUTH_SERVER_URL = process.env.AUTH_SERVER_URL || 'http://localhost:3000';
 
-module.exports = axios.create({
+const options = {
     baseURL: AUTH_SERVER_URL
-});
+};
+
+if(process.env.NODE_ENV === 'production'){
+    const httpsAgent = new https.Agent({
+        cert: fs.readFileSync(path.join(__dirname,'../../cert/akshitbansal_me.crt'))
+    });
+    options.httpsAgent = httpsAgent;
+}
+
+module.exports = axios.create(options);
